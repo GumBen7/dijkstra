da0143b6423e87ff00a7ff3ebab45937831ad830
Егор Сивцев <gumben7@gmail.com>
2022-12-17 11:03:45 +0700

    read and write array

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
 ﻿namespace ThreeThree {
     class Program {
         static void Main(string[] args) {
-            
+            var n = System.Int32.Parse(System.Console.ReadLine());
+            var sa = System.Console.ReadLine().Split();
+            var a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = System.Int32.Parse(sa[i]);
+            }
+            foreach (var j in a) {
+                System.Console.Write(j + " ");
+            }
+        }
+
+        static void NextPermut(int[] a, int n) {
+
         }
     }
 }
