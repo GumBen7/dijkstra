74fa19f71354dcc99ec70752029d8f5a9ea1b74f
Егор Сивцев <gumben7@gmail.com>
2022-12-17 12:07:16 +0700

    prefinal

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ThreeThree {
+﻿namespace FiveThree {
     class Program {
         static void Main(string[] args) {
             var n = System.Int32.Parse(System.Console.ReadLine());
@@ -7,13 +7,38 @@
             for (int i = 0; i < n; i++) {
                 a[i] = System.Int32.Parse(sa[i]);
             }
+            if (n > 1) {
+                NextPermut(ref a, ref n);
+            }
             foreach (var j in a) {
                 System.Console.Write(j + " ");
             }
         }
 
-        static void NextPermut(int[] a, int n) {
-
+        static void NextPermut(ref int[] a, ref int n) {
+            var i = n - 1;
+            var isSorted = false;
+            while (a[i] <= a[i-1]) {
+                --i;
+                if (i == 0) {
+                    isSorted = true;
+                    break;
+                }
+            }
+            var j = n - 1;
+            if (!isSorted) {                
+                while (a[i-1] >= a[j]) {
+                    --j;
+                }
+                (a[i-1], a[j]) = (a[j], a[i-1]);;
+            }
+            ++i;
+            j = n;
+            while (i < j) {
+                (a[i-1], a[j-1]) = (a[j-1], a[i-1]);
+                ++i;
+                --j;
+            }
         }
     }
 }
